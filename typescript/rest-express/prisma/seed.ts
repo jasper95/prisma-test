4c6dfd4a9185e0c953d94ccea9efeaf51c3503b5
Jasper Bernales <bernalesjasper@gmail.com>
2022-11-02 23:11:36 +0800

    final

--- typescript/rest-express/prisma/seed.ts
+++ typescript/rest-express/prisma/seed.ts
@@ -1,56 +1,133 @@
 import { PrismaClient, Prisma } from '@prisma/client'
-
 const prisma = new PrismaClient()
 
-// const userData: Prisma.UserCreateInput[] = [
-//   {
-//     name: 'Alice',
-//     waiter: {
-//       create: {
-//         name: 'Alice Waiter'
-//       },
-//     }
-//   },
-//   {
-//     name: 'Nilu',
-//     waiter: {
-//       create: {
-//         name: 'Nilu Waiter'
-//       }
-//     }
-//   },
-//   {
-//     name: 'Mahmoud',
-//     waiter: {
-//       create: {
-//         name: 'Mahmoud Waiter',
-//       },
-//     }
-//   },
-// ]
-
-const waiters: Prisma.WaiterCreateInput = [
+const userWaiter: Prisma.UserCreateInput[] = [1,2,3,4,5,6].map(i => (
   {
-    name: 'Alice Waiter',
+    name: `User Waiter ${i}`,
+    waiter: {
+      create: {
+        name: `User Waiter ${i}`,
+      },
+    }
+  }
+))
+
+const userChef: Prisma.UserCreateInput[] = [1,2,3,4,5,6].map(i => (
+  {
+    name: `User Chef ${i}`,
+    chef: {
+      create: {
+        name: `User Chef ${i}`,
+      },
+    }
+  }
+))
+
+
+const menuData: Prisma.MenuCreateInput[] = [1,2,3,4,5,6,].map(i => ({
+  content: `Menu ${i}`,
+    restaurants: {
+      createMany: {
+        data: [
+          {
+            name: `Restaurant ${i}`,
+          },
+        ]
+      }
+    },
+    menuItems: {
+      createMany: {
+        data: [
+          {
+            name: `Menu Item ${i}`
+          },
+        ]
+      }
+    }
+  }
+))
+const chefRestaurantData: Prisma.ChefsOnRestaurantsUncheckedCreateInput[] = [
+  {
+    chefId: 1,
+    restaurantId: 1
+  },
+  {
+    chefId: 2,
+    restaurantId: 2
+  },
+  {
+    chefId: 3,
+    restaurantId: 3
+  },
+  {
+    chefId: 4,
+    restaurantId: 4
+  },
+  {
+    chefId: 5,
+    restaurantId: 5
+  },
+  {
+    chefId: 6,
+    restaurantId: 6
+  }
+]
+
+const waiterRestaurantData: Prisma.WaitersOnRestaurantsUncheckedCreateInput[] = [
+  {
+    waiterId: 1,
+    restaurantId: 1
+  },
+  {
+    waiterId: 2,
+    restaurantId: 2
+  },
+  {
+    waiterId: 3,
+    restaurantId: 3
+  },
+  {
+    waiterId: 4,
+    restaurantId: 4
+  },
+  {
+    waiterId: 5,
+    restaurantId: 5
+  },
+  {
+    waiterId: 6,
+    restaurantId: 6
   }
 ]
 async function main() {
-  console.log(`Start seeding ...`)
-  prisma.restaurant.create({
-    data: {
-      waiters: {
-        create: waiters
-      }
-    }
-  })
-  // for (const u of userData) {
-  //   const user = await prisma.user.create({
-  //     data: u,
-  //   })
-  //   console.log(`Created user with id: ${user.id}`)
-  // }
-  console.log(`Seeding finished.`)
+  for(const data of userChef) {
+    await prisma.user.create({
+      data
+    })
+  }
+  for(const data of userWaiter) {
+    await prisma.user.create({
+      data
+    })
+  }
+  console.log('menuData: ', menuData.length);
+  for(const data of menuData) {
+    await prisma.menu.create({
+      data
+    })
+  }
+  for(const data of chefRestaurantData) {
+    await prisma.chefsOnRestaurants.create({
+      data
+    })
+  }
+  for(const data of waiterRestaurantData) {
+    await prisma.waitersOnRestaurants.create({
+      data
+    })
+  }
 }
+
 
 main()
   .then(async () => {
--- typescript/rest-express/src/index.ts
+++ typescript/rest-express/src/index.ts
@@ -1,3 +1,4 @@
+import pluralize from 'pluralize'
 import { Prisma, PrismaClient } from '@prisma/client'
 import express from 'express'
 
@@ -6,143 +7,199 @@ const app = express()
 
 app.use(express.json())
 
-app.post(`/signup`, async (req, res) => {
-  const { name, email, posts } = req.body
-
-  const postData = posts?.map((post: Prisma.PostCreateInput) => {
-    return { title: post?.title, content: post?.content }
-  })
-
-  const result = await prisma.user.create({
-    data: {
-      name,
-      email,
-      posts: {
-        create: postData,
-      },
-    },
-  })
-  res.json(result)
-})
-
-app.post(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      author: { connect: { email: authorEmail } },
-    },
-  })
-  res.json(result)
-})
-
-app.put('/post/:id/views', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    const post = await prisma.post.update({
-      where: { id: Number(id) },
-      data: {
-        viewCount: {
-          increment: 1,
-        },
-      },
-    })
-
-    res.json(post)
-  } catch (error) {
-    res.json({ error: `Post with ID ${id} does not exist in the database` })
+function buildFilter(queryPlan: any): any {
+  if(queryPlan.userId) {
+    return {
+      userId: queryPlan.userId
+    }
   }
-})
-
-app.put('/publish/:id', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    const postData = await prisma.post.findUnique({
-      where: { id: Number(id) },
-      select: {
-        published: true,
-      },
-    })
-
-    const updatedPost = await prisma.post.update({
-      where: { id: Number(id) || undefined },
-      data: { published: !postData?.published },
-    })
-    res.json(updatedPost)
-  } catch (error) {
-    res.json({ error: `Post with ID ${id} does not exist in the database` })
-  }
-})
-
-app.delete(`/post/:id`, async (req, res) => {
-  const { id } = req.params
-  const post = await prisma.post.delete({
-    where: {
-      id: Number(id),
-    },
-  })
-  res.json(post)
-})
-
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
-})
-
-app.get('/user/:id/drafts', async (req, res) => {
-  const { id } = req.params
-
-  const drafts = await prisma.user
-    .findUnique({
-      where: {
-        id: Number(id),
-      },
-    })
-    .posts({
-      where: { published: false },
-    })
-
-  res.json(drafts)
-})
-
-app.get(`/post/:id`, async (req, res) => {
-  const { id }: { id?: string } = req.params
-
-  const post = await prisma.post.findUnique({
-    where: { id: Number(id) },
-  })
-  res.json(post)
-})
-
-app.get('/feed', async (req, res) => {
-  const { searchString, skip, take, orderBy } = req.query
-
-  const or: Prisma.PostWhereInput = searchString
-    ? {
-        OR: [
-          { title: { contains: searchString as string } },
-          { content: { contains: searchString as string } },
-        ],
+  if(queryPlan.case === 'many-to-many') {
+    return {
+      [pluralize(queryPlan.to)]: {
+        some: {
+          [queryPlan.to]: buildFilter(queryPlan.in)
+        }
       }
-    : {}
+    }
+  }
+  if(queryPlan.case === 'many-to-one') {
+    return {
+      [pluralize(queryPlan.to)]: {
+        some: buildFilter(queryPlan.in)
+      }
+    }
+  }
+  return {
+    [queryPlan.to]: buildFilter(queryPlan.in)
+  }
+}
 
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
-      ...or,
-    },
-    include: { author: true },
-    take: Number(take) || undefined,
-    skip: Number(skip) || undefined,
-    orderBy: {
-      updatedAt: orderBy as Prisma.SortOrder,
-    },
+app.get('/user/:userId/chef/menuItems', async(req, res) => {
+  const { userId = 1 } = req.params
+  const queryPlan = {
+    from: 'menuItem',
+    fromField: 'menuItemId',
+    to: "menu",
+    toField: 'id',
+    case: "one-to-many",
+    in: {
+      from: 'menu',
+      fromField: 'menuId',
+      to: "restaurant",
+      toField: 'id',
+      case: "many-to-one",
+      in: {
+        from: 'restaurant',
+        fromField: 'restaurantId',
+        joinRelation: 'chefs_on_restaurant',
+        to: 'chef',
+        toField: 'chefId',
+        case: 'many-to-many',
+        in: {
+          userId: Number(userId),
+        }
+      }
+    }
+  }
+  const filter = buildFilter(queryPlan)
+  console.log('filter: ', JSON.stringify(filter));
+  const menus = await prisma.menuItem.findMany({
+    where: filter
   })
+  res.json(menus)
+})
 
-  res.json(posts)
+app.get('/user/:userId/waiter/menuItems', async(req, res) => {
+  const { userId = 1 } = req.params
+  const queryPlan = {
+    from: 'menuItem',
+    fromField: 'menuItemId',
+    to: "menu",
+    toField: 'id',
+    case: "one-to-many",
+    in: {
+      from: 'menu',
+      fromField: 'menuId',
+      to: "restaurant",
+      toField: 'id',
+      case: "many-to-one",
+      in: {
+        from: 'restaurant',
+        fromField: 'restaurantId',
+        joinRelation: 'waiters_on_restaurant',
+        to: 'waiter',
+        toField: 'waiterId',
+        case: 'many-to-many',
+        in: {
+          userId: Number(userId),
+        }
+      }
+    }
+  }
+  const filter = buildFilter(queryPlan)
+  console.log('filter: ', JSON.stringify(filter));
+  const menus = await prisma.menuItem.findMany({
+    where: filter
+  })
+  res.json(menus)
+})
+
+app.get('/user/:userId/chef/menus', async(req, res) => {
+  const { userId = 1 } = req.params
+  const queryPlan = {
+    from: 'menu',
+    fromField: 'menuId',
+    to: "restaurant",
+    toField: 'id',
+    case: "many-to-one",
+    in: {
+      from: 'restaurant',
+      fromField: 'restaurantId',
+      joinRelation: 'chefs_on_restaurant',
+      to: 'chef',
+      toField: 'chefId',
+      case: 'many-to-many',
+      in: {
+        userId: Number(userId),
+      }
+    }
+  }
+  const filter = buildFilter(queryPlan)
+  console.log('filter: ', JSON.stringify(filter));
+  const menus = await prisma.menu.findMany({
+    where: filter
+  })
+  res.json(menus)
+})
+
+app.get('/user/:userId/waiter/menus', async(req, res) => {
+  const { userId = 1 } = req.params
+  const queryPlan = {
+    from: 'menu',
+    fromField: 'menuId',
+    to: "restaurant",
+    toField: 'id',
+    case: "many-to-one",
+    in: {
+      from: 'restaurant',
+      fromField: 'restaurantId',
+      joinRelation: 'waiters_on_restaurant',
+      to: 'waiter',
+      toField: 'waiterId',
+      case: 'many-to-many',
+      in: {
+        userId: Number(userId),
+      }
+    }
+  }
+  const filter = buildFilter(queryPlan)
+  console.log('filter: ', JSON.stringify(filter));
+  const menus = await prisma.menu.findMany({
+    where: filter
+  })
+  res.json(menus)
+})
+
+app.get('/user/:userId/chef/restaurants', async(req, res) => {
+  const { userId = 1 } = req.params
+  const queryPlan = {
+    from: 'restaurant',
+    fromField: 'restaurantId',
+    joinRelation: 'chefs_on_restaurant',
+    to: 'chef',
+    toField: 'chefId',
+    case: 'many-to-many',
+    in: {
+      userId: Number(userId),
+    }
+  }
+  const filter = buildFilter(queryPlan)
+  console.log('filter: ', JSON.stringify(filter));
+  const menus = await prisma.restaurant.findMany({
+    where: buildFilter(queryPlan)
+  })
+  res.json(menus)
+})
+
+app.get('/user/:userId/waiter/restaurants', async(req, res) => {
+  const { userId = 1 } = req.params
+  const queryPlan = {
+    from: 'restaurant',
+    fromField: 'restaurantId',
+    joinRelation: 'waiters_on_restaurant',
+    to: 'waiter',
+    toField: 'waiterId',
+    case: 'many-to-many',
+    in: {
+      userId: Number(userId),
+    }
+  }
+  const filter = buildFilter(queryPlan)
+  console.log('filter: ', JSON.stringify(filter));
+  const menus = await prisma.restaurant.findMany({
+    where: filter
+  })
+  res.json(menus)
 })
 
 const server = app.listen(3000, () =>
