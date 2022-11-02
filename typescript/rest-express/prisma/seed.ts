8b346609b24b0157cc72e750ecd4d91047e891c6
Jasper Bernales <bernalesjasper@gmail.com>
2022-11-02 15:39:56 +0800

    schema

--- typescript/rest-express/prisma/seed.ts
+++ typescript/rest-express/prisma/seed.ts
@@ -2,60 +2,53 @@ import { PrismaClient, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: 'Alice',
-    email: 'alice@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Join the Prisma Slack',
-          content: 'https://slack.prisma.io',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Ask a question about Prisma on GitHub',
-          content: 'https://www.github.com/prisma/prisma/discussions',
-          published: true,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
-        },
-      ],
-    },
-  },
-]
+// const userData: Prisma.UserCreateInput[] = [
+//   {
+//     name: 'Alice',
+//     waiter: {
+//       create: {
+//         name: 'Alice Waiter'
+//       },
+//     }
+//   },
+//   {
+//     name: 'Nilu',
+//     waiter: {
+//       create: {
+//         name: 'Nilu Waiter'
+//       }
+//     }
+//   },
+//   {
+//     name: 'Mahmoud',
+//     waiter: {
+//       create: {
+//         name: 'Mahmoud Waiter',
+//       },
+//     }
+//   },
+// ]
 
+const waiters: Prisma.WaiterCreateInput = [
+  {
+    name: 'Alice Waiter',
+  }
+]
 async function main() {
   console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
-    })
-    console.log(`Created user with id: ${user.id}`)
-  }
+  prisma.restaurant.create({
+    data: {
+      waiters: {
+        create: waiters
+      }
+    }
+  })
+  // for (const u of userData) {
+  //   const user = await prisma.user.create({
+  //     data: u,
+  //   })
+  //   console.log(`Created user with id: ${user.id}`)
+  // }
   console.log(`Seeding finished.`)
 }
 
